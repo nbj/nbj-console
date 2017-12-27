a68314f2252600edfd7e269a8c9f3dfa4c3fb589
Nikolaj Boel Jensen <nbj@cego.dk>
2017-12-27 19:10:38 +0100

    Add more codes

--- src/Console/Command/NewProject.php
+++ src/Console/Command/NewProject.php
@@ -27,11 +27,34 @@ class NewProject extends Command
     protected $namespace;
 
     /**
+     * Holds whether or not project will be setup up of testing
+     *
+     * @var bool $useTesting
+     */
+    protected $useTesting = false;
+
+    /**
+     * Holds whether or not project will use PHP-CS-Fixer
+     *
+     * @var bool $usePHPCSFixer
+     */
+    protected $usePHPCSFixer = false;
+
+    /**
      * Holds the structure of the composer.json
      *
      * @var array $composerJsonStructure
      */
-    protected $composerJsonStructure;
+    protected $composerJsonStructure = [
+        'name'         => 'vendor/project',
+        'description'  => 'Project scaffold',
+        'type'         => 'project',
+        'license'      => 'MIT',
+        'require'      => [],
+        'require-dev'  => [],
+        'autoload'     => [],
+        'autoload-dev' => [],
+    ];
 
     /**
      * Configures the current command.
@@ -73,6 +96,16 @@ class NewProject extends Command
             $this->setupNamespacedAutoloading($input, $output);
         }
 
+        $testsQuestion = new ConfirmationQuestion('<question>Use PHPUnit for testing? (Y/n)</question> ');
+        if ($questionHelper->ask($input, $output, $testsQuestion)) {
+            $this->setupPHPUnitForTesting();
+        }
+
+        $codeStyleQuestion = new ConfirmationQuestion('<question>Use PHP-CS-Fixer for code style fixes? (Y/n)</question> ');
+        if ($questionHelper->ask($input, $output, $codeStyleQuestion)) {
+            $this->setupPHPCSFixer();
+        }
+
         $this->generateProject($input, $output);
     }
 
@@ -92,23 +125,70 @@ class NewProject extends Command
         $this->namespace = sprintf('%s\\', $namespace);
     }
 
+    /**
+     * Makes sure project is setup for testing
+     */
+    private function setupPHPUnitForTesting()
+    {
+        $this->useTesting = true;
+    }
+
+    /**
+     * Makes sure project is setup for cs fixes
+     */
+    private function setupPHPCSFixer()
+    {
+        $this->usePHPCSFixer = true;
+    }
+
+    /**
+     * Generates the project folder
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
     private function generateProject(InputInterface $input, OutputInterface $output)
     {
-        // Create namespace stub for composer.json
-        if ($this->namespace) {
-            $this->composerJsonStructure['autoload'] = [
-                'psr-4' => [
-                    $this->namespace => 'src/'
-                ]
-            ];
-        }
-
-        $composerJsonContent = json_encode($this->composerJsonStructure, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-
+        // Setup project directory
         $projectDir = sprintf("%s/%s", getcwd(), $this->projectName);
 
         mkdir($projectDir);
         chdir($projectDir);
+
+        // Create namespace stub in composer.json
+        if ($this->namespace) {
+            $this->composerJsonStructure['autoload']['psr-4'] = [
+                $this->namespace => 'src/'
+            ];
+        }
+
+        // Create test namespace stub in composer.json
+        if ($this->useTesting) {
+            $this->composerJsonStructure['autoload-dev']['psr-4'] = [
+                'Tests\\' => 'tests/'
+            ];
+
+            $this->composerJsonStructure['require-dev']['phpunit/phpunit'] = '*';
+
+            $fileContent = file_get_contents(STUBS_PATH . '/phpunit');
+            file_put_contents('phpunit.xml', $fileContent, FILE_BINARY);
+        }
+
+        // Create test namespace stub in composer.json
+        if ($this->usePHPCSFixer) {
+            $this->composerJsonStructure['require-dev']['friendsofphp/php-cs-fixer'] = '*';
+
+            $fileContent = file_get_contents(STUBS_PATH . '/php_cs_fixer');
+            file_put_contents('.php_cs', $fileContent, FILE_BINARY);
+        }
+
+        foreach ($this->composerJsonStructure as $key => $value) {
+            if (is_array($value) && empty($value)) {
+                $this->composerJsonStructure[$key] = (object) [];
+            }
+        }
+
+        $composerJsonContent = json_encode($this->composerJsonStructure, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 
         file_put_contents('composer.json', $composerJsonContent, FILE_BINARY);
     }
